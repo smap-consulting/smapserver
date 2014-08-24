cd0ae25c0b2abfbf5b9b04689fc05fa698e7f214
Neil Penman <neilpenman@gmail.com>
2014-08-24 18:17:29 +0700

    Added analysis menu to edit page.  Made panels.js dependent on script.js

--- fieldAnalysis/WebContent/js/app/panels.js
+++ fieldAnalysis/WebContent/js/app/panels.js
@@ -22,7 +22,7 @@ along with SMAP.  If not, see <http://www.gnu.org/licenses/>.
 
 // HTML fragments for constructing panels
 
-define(['jquery', 'jquery_ui', 'localise', 'common', 'globals'], function($, ui, lang, common, globals) {
+define(['jquery', 'jquery_ui', 'localise', 'common', 'globals','app/script'], function($, ui, lang, common, globals) {
 	
 var hstart = '<li class="ui-state-default pSmall" id="p';
 var hstart2 = '">';
