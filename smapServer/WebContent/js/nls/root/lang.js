c1fac570fc1fd3488041b2e0882ce241e05fb519
Neil Penman <neilpenman@gmail.com>
2014-12-30 08:49:21 +1100

    Minor changes to bootstrap implementation

--- smapServer/WebContent/js/nls/root/lang.js
+++ smapServer/WebContent/js/nls/root/lang.js
@@ -18,6 +18,7 @@ define({
 	"m_locations": "Locations",
 	"m_section": "Section",
 	"m_notify": "Notifications",
+	"m_view": "Viewxx",
 	
 	// Smap Server
 	"ss_welcome": "Welcome to Smap Server Version:",
