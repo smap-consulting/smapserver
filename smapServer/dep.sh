ddc803d94600b5e6fec248ba3bfa4431e78be11f
Neil Penman <neilpenman@gmail.com>
2015-03-14 09:57:43 +1100

    Modified WebForm to work with evolving webformClient

--- smapServer/dep.sh
+++ smapServer/dep.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# Get latest webformsClient
+cp ~/git/webformsClient/public/build/js/webform-combined.min.js WebContent/build/js/webform-combined.min.js
+cp -rf  ~/git/webformsClient/public/css WebContent/build
+cp -rf  ~/git/webformsClient/public/fonts WebContent/build
+
 # Minify
 node tools/r.js -o tools/build.js
 
--- smapServer/enk_up.sh
+++ smapServer/enk_up.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
-#cp ~/git/enketo-core/build/js/combined.min.js WebContent/js/libs
-
 
 # Get enketo-core1
 #cp ~/git/webforms/public/build/js/webform-combined.min.js WebContent/build/js/webform-combined.min.js
 
 # Get enketo-core2
-cp ~/git/webforms2/public/build/js/webform-combined.min.js WebContent/build/js/webform-combined.min.js
-cp -rf  ~/git/webforms2/public/css WebContent/build
-cp -rf  ~/git/webforms2/public/fonts WebContent/build
+cp ~/git/webformsClient/public/build/js/webform-combined.min.js WebContent/build/js/webform-combined.min.js
+cp -rf  ~/git/webformsClient/public/css WebContent/build
+cp -rf  ~/git/webformsClient/public/fonts WebContent/build
