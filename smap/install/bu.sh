a0e26a327b2199693fd00c5aab02aa408b94969a
Neil Penman <neilpenman@gmail.com>
2016-01-25 16:27:49 +1100

    fixes to issues found in pre-release testing

--- smap/install/bu.sh
+++ smap/install/bu.sh
@@ -6,8 +6,8 @@ rm $file
 rm $file.gpg
 
 # Dump databases
-pg_dump -c -Fc survey_definitions > sd.dmp
-pg_dump -c -Fc results > results.dmp
+pg_dump -c -Fc survey_definitions > backups/sd.dmp
+pg_dump -c -Fc results > backups/results.dmp
 pg_dump survey_definitions -c > backups/survey_definitions.sql
 pg_dump results -c > backups/results.sql
 
