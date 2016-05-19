15a235f0fbc41631fbcbf44b92ad93ea688d6e05
Neil Penman <neilpenman@gmail.com>
2016-05-20 08:03:59 +1000

    Fixed issues related to the release of downloadable version

--- smapServer/WebContent/js/resources.js
+++ smapServer/WebContent/js/resources.js
@@ -225,6 +225,7 @@ function saveMap() {
 		  type: "POST",
 		  async: false,
 		  url: url,
+		  contentType: "application/json",
 		  data: { map: mapString },
 		  success: function(data, status) {
 			  removeHourglass();
