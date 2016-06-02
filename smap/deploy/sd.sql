8bde012fc69df37bdc9e1a6fa4c17905fe98332f
Neil Penman <neilpenman@gmail.com>
2016-06-03 09:50:00 +1000

    Update patch script to version 160501

--- smap/deploy/sd.sql
+++ smap/deploy/sd.sql
@@ -503,4 +503,4 @@ alter table tasks alter column schedule_at type timestamp;
 alter table tasks add column repeat_count integer default 0;
 
 -- Upgrade to 16.06 from 16.05
-alter table survey add column hrk text;		// Human readable key
+alter table survey add column hrk text;
