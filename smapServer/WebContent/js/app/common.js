f6eae733efa7a3009ab12b33e52d1b18e602b140
Neil Penman <neilpenman@gmail.com>
2016-10-31 15:16:31 +0545

    Set signature path correctly

--- smapServer/WebContent/js/app/common.js
+++ smapServer/WebContent/js/app/common.js
@@ -303,7 +303,7 @@ function updateUserDetails(data, getOrganisationsFn) {
 		var userDetails = JSON.parse(data.settings);
 		$('#my_title').val(userDetails.title);
 		$('#my_license').val(userDetails.license);
-		$('#my_signature').attr("src", data.signature);
+		$('#my_signature').attr("src", "/surveyKPI/file/" + data.signature + "/users?type=sig");
 	}
 	
 	// Hide any menus that have been disabled by custom java scripts
