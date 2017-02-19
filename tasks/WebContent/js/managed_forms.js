59aaab2ca20dd941d48d33e8def3b08e56ae1cef
Neil Penman <neilpenman@gmail.com>
2017-02-19 13:06:54 +0800

    export chart data and settings from browse results

--- tasks/WebContent/js/managed_forms.js
+++ tasks/WebContent/js/managed_forms.js
@@ -393,11 +393,48 @@ require([
 		 	filename,
 		 	mime,
 			data,
+			settings = [],
 		 	managedId,
 		 	title = $('#survey_name option:selected').text(),
 		 	project = $('#project_name option:selected').text(),
 		 	charts = [],
-		 	chartData;
+		 	chartData,
+		 	settingsObj,
+		 	fromVal,
+		 	toVal,
+		 	colCount = 0,
+		 	colName,
+		 	colValue,
+		 	i;
+		 
+		 /*
+		  * Get the settings
+		  */
+		 settingsObj = globals.gMainTable.settings();
+		 settings.push({
+			 k: "search",
+			 v: settingsObj.search()
+		 })
+		 settings.push({
+			k: "from",
+			v: $('#filter_from').val()
+		 });
+		 settings.push({
+			k: "to",
+			v: $('#filter_to').val()
+		 });
+		 colCount = globals.gMainTable.columns()[0].length;
+		 for(i = 0; i < colCount; i++) {
+			 colValue = globals.gMainTable.column(i).search();
+			 if(colValue && colValue.trim().length > 2) {
+				 
+				 settings.push({
+						k: $(globals.gMainTable.column(i).header()).find('span').text(),
+						v: colValue.substring(1, colValue.length - 1)	// Remove regexp
+					 });
+			 }
+			
+		 }
 		 
 		 
 		 data = getTableData(globals.gMainTable, 
@@ -427,9 +464,8 @@ require([
 			 if(format === "xlsx") {
 				 chartData = chart.refreshCharts(true);
 			 }
-			 console.log(chartData);
 			 
-			 generateFile(url, filename, format, mime, data, globals.gCurrentSurvey, managedId, title, project, charts, chartData);
+			 generateFile(url, filename, format, mime, data, globals.gCurrentSurvey, managedId, title, project, charts, chartData, settings);
 		 } else {
 			 countImages = $('.svg-container svg').length;
 			 $('.svg-container svg').each(function(index) {
@@ -450,7 +486,7 @@ require([
 					 console.log("Got image: " + countImages);
 					 countImages--;
 					 if(countImages <= 0) {
-						 generateFile(url, filename, format, mime, undefined, globals.gCurrentSurvey, managedId, title, project, charts, chartData);
+						 generateFile(url, filename, format, mime, undefined, globals.gCurrentSurvey, managedId, title, project, charts, chartData, settings);
 					 }
 				 });
 				
