4e82e029e332d8cce9f07c0f435986ee5a417c4b
Neil Penman <neilpenman@gmail.com>
2017-06-17 15:13:27 +0200

    Use choice labels in wordcloud

--- tasks/WebContent/js/app/chart.js
+++ tasks/WebContent/js/app/chart.js
@@ -428,14 +428,14 @@ define([
                 // Normalise 2 dimensional array
                 for(i = 0; i < newData.length; i++) {
 
-                    var choiceLabel = newData[i].key;
-                    if(chart.groups.length === 1 && (chart.groups[0].type == 'select' || chart.groups[0].type == 'select1')) {
-                        choiceLabel = lookupChoiceLabel(chart.groups[0].l_id, newData[i].key);
-                    } else  if(chart.groups.length === 2 && (chart.groups[1].type == 'select' || chart.groups[1].type == 'select1')) {
-                        choiceLabel = lookupChoiceLabel(chart.groups[1].l_id, newData[i].key);
-                    }
+                    //var choiceLabel = newData[i].key;
+                    //if(chart.groups.length === 1 && (chart.groups[0].type == 'select' || chart.groups[0].type == 'select1')) {
+                    //    choiceLabel = lookupChoiceLabel(chart.groups[0].l_id, newData[i].key);
+                    //} else  if(chart.groups.length === 2 && (chart.groups[1].type == 'select' || chart.groups[1].type == 'select1')) {
+                    //    choiceLabel = lookupChoiceLabel(chart.groups[1].l_id, newData[i].key);
+                    //}
                     var item = {
-                        key: choiceLabel,
+                        key: newData[i].key,
                         pr: []
                     };
 
@@ -452,7 +452,7 @@ define([
                                 if(newData[i].values[k].key === columnArray[j]) {
                                     item.pr.push({
                                         key: columnArray[j],
-                                        value: newData[i].values[k].values
+                                        value: newData[i].values[k].value
                                     });
                                     hasValue = true;
                                     break;
@@ -505,7 +505,6 @@ define([
                     for (i = 0; i < results.length; i++) {
                         var di = {};
                         di.count = 1;
-                        //for (var p in results[i]) {
                         for (j = 0; j < columns.length; j++) {
                             var val = results[i][columns[j].name];
                             if(columns[j].l_id > 0) {
@@ -688,7 +687,11 @@ define([
             var common = "i,me,my,myself,we,us,our,ours,ourselves,you,your,yours,yourself,yourselves,he,him,his,himself,she,her,hers,herself,it,its,itself,they,them,their,theirs,themselves,what,which,who,whom,whose,this,that,these,those,am,is,are,was,were,be,been,being,have,has,had,having,do,does,did,doing,will,would,should,can,could,ought,i'm,you're,he's,she's,it's,we're,they're,i've,you've,we've,they've,i'd,you'd,he'd,she'd,we'd,they'd,i'll,you'll,he'll,she'll,we'll,they'll,isn't,aren't,wasn't,weren't,hasn't,haven't,hadn't,doesn't,don't,didn't,won't,wouldn't,shan't,shouldn't,can't,cannot,couldn't,mustn't,let's,that's,who's,what's,here's,there's,when's,where's,why's,how's,a,an,the,and,but,if,or,because,as,until,while,of,at,by,for,with,about,against,between,into,through,during,before,after,above,below,to,from,up,upon,down,in,out,on,off,over,under,again,further,then,once,here,there,when,where,why,how,all,any,both,each,few,more,most,other,some,such,no,nor,not,only,own,same,so,than,too,very,say,says,said,shall";
 
             var textArray = results.map(function (d) {
-                return d[chart.groups[0].name];
+                if(chart.groups[0].l_id > 0) {
+                    return lookupChoiceLabel(chart.groups[0].l_id, d[chart.groups[0].name]);
+                } else {
+                    return d[chart.groups[0].name];
+                }
             });
 
             var data = {};
